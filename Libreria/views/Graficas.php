64d1cea897d0a3684e19c3aa6edf05d71da828fc
CarlosOrd11 <80226125+CarlosOrd11@users.noreply.github.com>
2021-04-15 21:52:52 -0600

    Se actualizo la página de gráficas

--- Libreria/views/Graficas.php
+++ Libreria/views/Graficas.php
@@ -9,8 +9,8 @@ Dashboard_Page::headerTemplate('libreria');
             <!--Defiendo el contenido de la card que contendrá las gráficas-->
             <div class="card-content black-text">
                 <!--Definiendo el nombre del encabezado-->
-                <h3 class="center-align">Benvenido Usuario</h3>
-                <h5 class="center-align">Estas son las novedades:</h5>
+                <h3 class="center-align Titulos yellow-text text-darken-3">Benvenido Usuario - Gerente</h3>
+                <h5 class="center-align Text">Estas son las novedades:</h5>
                 <!--Definiendo el panel número 1 para almacenar las gráficas-->
                 <!--En este caso solo son imagenes-->
             </div>
@@ -18,13 +18,13 @@ Dashboard_Page::headerTemplate('libreria');
         <div class="col s12 m12">
             <div class="card-panel white">
                 <div class="row">
-                    <div class="col s12 m6 center-align">                        
+                    <div class="col s12 m6 center-align">                                            
                     <br>
-                    <br>
-                        <img class="responsive-img" src="../resources/img/productos/descarga.png">
+                        <img class="responsive-img" src="../resources/img/productos/chart1.PNG">
                     </div>
-                    <div class="col s12 m6 center-align">
-                        <img class="responsive-img" src="../resources/img/productos/grafica2.png">
+                    <div class="col s12 m6 center-align">                    
+                    <br>
+                        <img class="responsive-img" src="../resources/img/productos/chart2.PNG">
                     </div>
                 </div>
             </div>
