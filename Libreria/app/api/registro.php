0f06383376f99fb3439343ebee4d6b37f6067b7e
OVillalobos3A <20190474@ricaldone.edu.sv>
2021-07-04 20:02:58 -0600

    Actualización de métodos para el apartado de usuarios.

---
+++ Libreria/app/api/registro.php
@@ -0,0 +1,211 @@
+<?php
+require_once('../helpers/database.php');
+require_once('../helpers/validator.php');
+require_once('../models/usuarios.php');
+require_once('../models/registro.php');
+
+// Se comprueba si existe una acción a realizar, de lo contrario se finaliza el script con un mensaje de error.
+if (isset($_GET['action'])) {
+    // Se crea una sesión o se reanuda la actual para poder utilizar variables de sesión en el script.
+    session_start();
+    // Se instancia la clase correspondiente.
+    $usuario = new Usuarios;
+    $registro = new Registro;
+    // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
+    $result = array('status' => 0, 'error' => 0, 'message' => null, 'exception' => null);
+    // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
+    if (isset($_SESSION['id_usuario'])) {
+        // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
+        switch ($_GET['action']) {
+            case 'logOut':
+                if (session_destroy()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Sesión eliminada correctamente';
+                } else {
+                    $result['exception'] = 'Ocurrió un problema al cerrar la sesión';
+                }
+                break;
+            case 'readAll':
+                if ($result['dataset'] = $usuario->readAll()) {
+                    $result['status'] = 1;
+                } else {
+                    if (Database::getException()) {
+                        $result['exception'] = Database::getException();
+                    } else {
+                        $result['exception'] = 'No hay usuarios registrados';
+                    }
+                }
+                break;
+            case 'readOne':
+                if ($usuario->setId($_POST['id_usuario'])) {
+                    if ($result['dataset'] = $usuario->readOne()) {
+                        $result['status'] = 1;
+                    } else {
+                        if (Database::getException()) {
+                            $result['exception'] = Database::getException();
+                        } else {
+                            $result['exception'] = 'Usuario inexistente';
+                        }
+                    }
+                } else {
+                    $result['exception'] = 'Usuario incorrecto';
+                }
+                break;
+            default:
+                $result['exception'] = 'Acción no disponible dentro de la sesión';
+        }
+    }else {
+        // Se compara la acción a realizar cuando el administrador no ha iniciado sesión.
+        switch ($_GET['action']) {
+            case 'readAll':
+                if ($usuario->readAll()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Existe al menos un usuario registrado';
+                } else {
+                    if (Database::getException()) {
+                        $result['exception'] = Database::getException();
+                    } else {
+                        $result['exception'] = 'No existen usuarios registrados';
+                    }
+                }
+                break;
+            case 'register':
+                $_POST = $registro->validateForm($_POST);
+                if ($registro->setNombres($_POST['nombre'])) {
+                    if ($registro->setApellidos($_POST['apellido'])) {
+                        if ($registro->setCorreo($_POST['correo'])) {
+                            if ($registro->setTelefono($_POST['telefono'])) {
+                                if ($registro->setNacimiento($_POST['fecha'])) {
+                                    if (isset($_POST['genero'])) {
+                                        if ($registro->setGenero($_POST['genero'])) {
+                                            if($registro->setDui($_POST['dui'])){
+                                                if ($registro->setUsuario($_POST['alias'])) {         
+                                                    if ($_POST['contra'] == $_POST['contra2']) {
+                                                        if ($registro->setClave($_POST['contra'])) {
+                                                            if (isset($_POST['tipo'])) {
+                                                                if($registro->setTipo($_POST['tipo'])){
+                                                                    if (is_uploaded_file($_FILES['foto']['tmp_name'])) {
+                                                                        if ($registro->setImagen($_FILES['foto'])) {
+                                                                            if ($registro->createRow()) {
+                                                                                if ($registro->saveFile($_FILES['foto'], $registro->getRuta(), $registro->getImagen())) {
+                                                                                    $result['message'] = 'Producto creado correctamente';
+                                                                                    if ($registro->createUser()) {
+                                                                                        $result['status'] = 1;
+                                                                                        $result['message'] = 'Usuario creado correctamente';
+                                                                                    } else {
+                                                                                    $result['exception'] = Database::getException();
+                                                                                    }
+                                                                                } else {
+                                                                                    $result['message'] = 'Producto creado pero no se guardó la imagen';
+                                                                                    if ($registro->createUser()) {
+                                                                                        $result['status'] = 1;
+                                                                                        $result['message'] = 'Usuario creado correctamente';
+                                                                                    } else {
+                                                                                        $result['exception'] = Database::getException();
+                                                                                    }
+                                                                                }
+                                                                            } else {
+                                                                                $result['exception'] = Database::getException();
+                                                                                $registro->deleteAll();
+                                                                                if ($registro->createRow()) {
+                                                                                    if ($registro->saveFile($_FILES['foto'], $registro->getRuta(), $registro->getImagen())) {
+                                                                                        $result['message'] = 'Producto creado correctamente';
+                                                                                        if ($registro->createUser()) {
+                                                                                            $result['status'] = 1;
+                                                                                            $result['message'] = 'Usuario creado correctamente';
+                                                                                        } else {
+                                                                                        $result['exception'] = Database::getException();
+                                                                                        }
+                                                                                    } else {
+                                                                                        $result['message'] = 'Producto creado pero no se guardó la imagen';
+                                                                                        if ($registro->createUser()) {
+                                                                                            $result['status'] = 1;
+                                                                                            $result['message'] = 'Usuario creado correctamente';
+                                                                                        } else {
+                                                                                            $result['exception'] = Database::getException();
+                                                                                        }
+                                                                                    }
+                                                                                } else {
+                                                                                    $result['exception'] = Database::getException();
+                                                                                }
+                                                                            }
+                                                                        } else {
+                                                                            $result['exception'] = $registro->getImageError();
+                                                                        }
+                                                                    } else {
+                                                                        $result['exception'] = 'Seleccione una imagen';
+                                                                    }
+                                                                } else{
+                                                                    $result['exception'] = 'Tipo del usuario incorrecto';
+                                                                }
+                                                            } else {
+                                                                $result['exception'] = 'Seleccione un Genero';
+                                                            }
+                                                        } else {
+                                                            $result['exception'] = $usuario->getPasswordError();
+                                                        }
+                                                    } else {
+                                                        $result['exception'] = 'Claves diferentes';
+                                                    } 
+                                                } else {
+                                                    $result['exception'] = 'Alias incorrecto';
+                                                }
+                                            }else{
+                                                $result['exception'] = 'DUI incorrecto';
+                                            }
+                                        } else {
+                                            $result['exception'] = $registro->getPasswordError();
+                                        }
+                                    } else {
+                                        $result['exception'] = 'Seleccione una categoría';
+                                    }
+                                } else {
+                                    $result['exception'] = 'Claves diferentes';
+                                }
+                            } else {
+                                $result['exception'] = 'Telefono incorrecto';
+                            }
+                        } else {
+                            $result['exception'] = 'Correo incorrecto';
+                        }
+                    } else {
+                        $result['exception'] = 'Apellidos incorrectos';
+                    }
+                } else {
+                    $result['exception'] = 'Nombres incorrectos';
+                }
+                break;
+            case 'logIn':
+                $_POST = $usuario->validateForm($_POST);
+                if ($usuario->checkUser($_POST['user'])) {
+                    if ($usuario->checkPassword($_POST['pass'])) {
+                        $result['status'] = 1;
+                        $result['message'] = 'Autenticación correcta';
+                        $_SESSION['id_usuario'] = $usuario->getId();
+                        $_SESSION['alias_usuario'] = $usuario->getAlias();
+                    } else {
+                        if (Database::getException()) {
+                            $result['exception'] = Database::getException();
+                        } else {
+                            $result['exception'] = 'Clave incorrecta';
+                        }
+                    }
+                } else {
+                    if (Database::getException()) {
+                        $result['exception'] = Database::getException();
+                    } else {
+                        $result['exception'] = 'Alias incorrecto';
+                    }
+                }
+                break;
+            default:
+                $result['exception'] = 'Acción no disponible fuera de la sesión';
+        }
+    } 
+    // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
+    header('content-type: application/json; charset=utf-8');
+    // Se imprime el resultado en formato JSON y se retorna al controlador.
+    print(json_encode($result));
+} else {
+    print(json_encode('Recurso no disponible'));
+}
---
+++ Libreria/app/controllers/account.js
@@ -0,0 +1,77 @@
+/*
+*   Este controlador es de uso general en las páginas web del sitio privado. Se importa en la plantilla del pie del documento.
+*   Sirve para manejar todo lo que tiene que ver con la cuenta del usuario.
+*/
+
+// Constante para establecer la ruta y parámetros de comunicación con la API.
+const API = '../app/api//usuarios.php?action=';
+
+// Función para mostrar el formulario de editar perfil con los datos del usuario que ha iniciado sesión.
+function openProfileDialog() {
+    // Se abre la caja de dialogo (modal) que contiene el formulario para editar perfil, ubicado en el archivo de las plantillas.
+    let instance = M.Modal.getInstance(document.getElementById('profile-modal'));
+    instance.open();
+
+    fetch(API + 'readProfile', {
+        method: 'get'
+    }).then(function (request) {
+        // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje indicando el problema.
+        if (request.ok) {
+            request.json().then(function (response) {
+                // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+                if (response.status) {
+                    // Se inicializan los campos del formulario con los datos del usuario que ha iniciado sesión.
+                    document.getElementById('nombres_perfil').value = response.dataset.nombres_usuario;
+                    document.getElementById('apellidos_perfil').value = response.dataset.apellidos_usuario;
+                    document.getElementById('correo_perfil').value = response.dataset.correo_usuario;
+                    document.getElementById('alias_perfil').value = response.dataset.alias_usuario;
+                    // Se actualizan los campos para que las etiquetas (labels) no queden sobre los datos.
+                    M.updateTextFields();
+                } else {
+                    sweetAlert(2, response.exception, null);
+                }
+            });
+        } else {
+            console.log(request.status + ' ' + request.statusText);
+        }
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
+// Función para mostrar un mensaje de confirmación al momento de cerrar sesión.
+function logOut() {
+    swal({
+        title: 'Advertencia',
+        text: '¿Quiere cerrar la sesión?',
+        icon: 'warning',
+        buttons: ['No', 'Sí'],
+        closeOnClickOutside: false,
+        closeOnEsc: false
+    }).then(function (value) {
+        // Se verifica si fue cliqueado el botón Sí para hacer la petición de cerrar sesión, de lo contrario se muestra un mensaje.
+        if (value) {
+            fetch(API + 'logOut', {
+                method: 'get'
+            }).then(function (request) {
+                // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje indicando el problema.
+                if (request.ok) {
+                    request.json().then(function (response) {
+                        // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
+                        if (response.status) {
+                            sweetAlert(1, response.message, 'index.php');
+                        } else {
+                            sweetAlert(2, response.exception, null);
+                        }
+                    });
+                } else {
+                    console.log(request.status + ' ' + request.statusText);
+                }
+            }).catch(function (error) {
+                console.log(error);
+            });
+        } else {
+            sweetAlert(4, 'Puede continuar con la sesión', null);
+        }
+    });
+}
