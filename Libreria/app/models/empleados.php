926de43d7518ea9f81fbdb6f67008cb78b880eae
Alepo2412 <55759742+alepo2412@users.noreply.github.com>
2021-08-07 16:48:56 -0600

    Preparando Proyecto

--- Libreria/app/models/empleados.php
+++ Libreria/app/models/empleados.php
@@ -91,7 +91,8 @@ class Empleados extends Validator
     //Permite validar la Fecha ingresada para el Empleado
     public function setFecha($value)
     {
-        if ($this->validateDate($value)) {
+        //Se valida que la fecha ingresada corresponda con la mayoría de Edad, a la vez que se comprueba el formato ingresado
+        if ($this->validateBirthDate($value)) {
             $this->fecha = $value;
             return true;
         } else {
@@ -99,8 +100,10 @@ class Empleados extends Validator
         }
     }
 
+    //Se valida el Género del Empleado
     public function setGen($value)
     {
+        //Se valida que el género ingresado cumpla con el formato establecido
         if ($this->validateGen($value)) {
             //Se guarda el dato
             $this->gen = $value;
@@ -110,8 +113,10 @@ class Empleados extends Validator
         }
     }
 
+    //Se valida el DUI del Empleado
     public function setDui($value)
     {
+        //Se valida que el campo tenga el formato requerido
         if ($this->validateDUI($value, 1, 50)) {
             //Se guarda el dato
             $this->dui = $value;
@@ -121,8 +126,10 @@ class Empleados extends Validator
         }
     }
 
+    //Se valida el Código de Recuperación del Usuario
     public function setCodigo($value)
     {
+        //Se valida que el campo sea un número natural
         if ($this->validateNaturalNumber($value)) {
             //Se guarda el dato
             $this->primer_uso = $value;
@@ -132,8 +139,10 @@ class Empleados extends Validator
         }
     }
 
+    //Se valida el Estado del Usuario
     public function setEstado($value)
     {
+        //Se valida que el campo sea alfanumérico
         if ($this->validateAlphanumeric($value, 1, 50)) {
             //Se guarda el dato
             $this->estado = $value;
@@ -143,8 +152,10 @@ class Empleados extends Validator
         }
     }
 
+    //Se valida la imagen
     public function setImagen($file)
     {
+        //Se comprueban los parámetros de entrada necesarios para poder guardas la imagen
         if ($this->validateImageFile($file, 500, 500)) {
             //Se guarda el dato
             $this->imagen = $this->getImageName();
@@ -158,100 +169,141 @@ class Empleados extends Validator
     
     //Métodos para obtener valores de los atributos.
     
+    //Se obtiene el Id del Empleado
     public function getId()
     {
+        //Se retorna el dato
         return $this->id;
     }
+
+    //Se obtiene el Nombre del Empleado
     public function getNombre()
     {
+        //Se retorna el dato
         return $this->nombre;
     }
 
+    //Se obtienen los Apellidos del Empleado
     public function getApellido()
     {
+        //Se retorna el dato
         return $this->apellido;
     }
   
+    //Se obtiene el Correo Electrónico del Empleado
     public function getCorreo()
     {
+        //Se retorna el dato
         return $this->correo;
     }
 
+    //Se obtiene la Fecha de Nacimiento del Empleado
     public function getFecha()
     {
+        //Se retorna el dato
         return $this->fecha_nac;
     }
 
+    //Se obtiene el Teléfono del Empleado
     public function getTel()
     {
+        //Se retorna el dato
         return $this->telefono;
     }
 
+    //Se obtiene el Género del Empleado
     public function getGenero()
     {
+        //Se retorna el dato
         return $this->genero;
     }
 
+    //Se obtiene el DUI del Empleado
     public function getDui()
     {
+        //Se retorna el dato
         return $this->dui;
     }
 
+    //Se obtiene el código de Recuperación del Empleado
     public function getCodigo()
     {
+        //Se retorna el dato
         return $this->codigo_recu;
     }
 
+    //Se obtiene el Estado del Empleado
     public function getEstado()
     {
+        //Se retorna el dato
         return $this->estado;
     }
 
+    //Se obtiene la Imagen del Empleado
     public function getImagen()
     {
+        //Se retorna el dato
         return $this->imagen;
     }
 
+    //Se obtiene la Ruta de la Imagen del Empleado
     public function getRuta()
     {
+        //Se retorna el dato
         return $this->ruta;
     }
 
     
       //Métodos para realizar las operaciones SCRUD (search, create, read, update, delete).
     
-
+    //Ver todos los Empleados
     public function readAll()
-    {
+    {   
+        //Se guarda la consulta sql
         $sql = 'SELECT id_empleado, nombre, apellido, correo, dui, telefono, genero, fecha_nac, empleados.estado, imagen
                 FROM empleados';
+        //Se guarda un array con los parametros de la consulta(vacío)
         $params = null;
+        //Se retorna la ejecución del método "getRows"
         return Database::getRows($sql, $params);
     }
     
+    //Ver un empleado en específico
     public function readOne()
     {
+        //Se guarda la consulta sql
         $sql = 'SELECT id_empleado, nombre, apellido, correo, dui, telefono, genero, fecha_nac, estado, imagen
                 FROM empleados
-                WHERE id_empleado = ?';        
+                WHERE id_empleado = ?';     
+        //Se guarda un array con los parametros de la consulta   
         $params = array($this->id);        
+        //Se retorna la ejecución del método "getRow"
         return Database::getRow($sql, $params);
     }
 
+    //Buscar
     public function searchRows($value)
     {
+        //Se guarda la consulta sql
         $sql = 'SELECT id_empleado, nombre, apellido, correo, dui, telefono, estado, genero, fecha_nac, imagen
                 FROM empleados
                 WHERE nombre ILIKE ? and id_empleado <> (Select MIN(id_empleado)from empleados)';
+        //Se guarda un array con los parametros de la consulta
         $params = array("%$value%");
+        //Se retorna la ejecución del método "getRows"
         return Database::getRows($sql, $params);
     }
 
+    //Crear
     public function createRow()
     {
+        //Se guarda la consulta sql
         $sql = 'INSERT INTO empleados(imagen, nombre, apellido, correo, telefono, dui, fecha_nac, genero, estado)
                 VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)';
+        //Se guarda un array con los parametros de la consulta
         $params = array($this->imagen, $this->nombre, $this->apellido, $this->correo, $this->tel, $this->dui, $this->fecha, $this->gen, $this->estado);
+        
+        //Se retorna la ejecución del método "getLastRow"
         if ($this->ide = Database::getLastRow($sql, $params)) {
             return true;
         } else {
@@ -259,33 +311,42 @@ class Empleados extends Validator
         }
     }
 
+    //Contador
     public function readCount()
     {
+        //Se guarda la consulta sql
         $sql = 'SELECT Count(id_empleado) as numero
                 FROM empleados';
+        //Se guarda un array con los parametros de la consulta(vacío)        
         $params = null;
+        //Se retorna la ejecución del método "getRow"
         return Database::getRow($sql, $params);
     }
 
-    
-
+    //Actualizar
     public function updateRow($current_image)
     {
         // Se verifica si existe una nueva imagen para borrar la actual, de lo contrario se mantiene la actual.
         ($this->imagen) ? $this->deleteFile($this->getRuta(), $current_image) : $this->imagen = $current_image;
-
+        //Se guarda la consulta sql
         $sql = 'UPDATE empleados 
                 SET nombre = ?, apellido = ?, correo = ?, telefono = ?, estado = ?, fecha_nac = ?, genero = ?, dui = ?, imagen = ?
                 WHERE id_empleado= ?';
+        //Se guarda un array con los parametros de la consulta
         $params = array($this->nombre, $this->apellido, $this->correo, $this->tel, $this->estado, $this->fecha, $this->gen, $this->dui, $this->imagen, $this->id);
+        //Se retorna la ejecución del método "executeRow"
         return Database::executeRow($sql, $params);
     }
 
+    //Eliminar
     public function deleteRow()
     {
+        //Se guarda la consulta sql
         $sql = 'DELETE FROM empleados
                 WHERE id_empleado = ?';
+        //Se guarda un array con los parametros de la consulta(vacío)  
         $params = array($this->id);
+        //Se retorna la ejecución del método "executeRow"
         return Database::executeRow($sql, $params);
     }
 
--- Libreria/app/models/entradas.php
+++ Libreria/app/models/entradas.php
@@ -1,6 +1,6 @@
 <?php
 /*
-*	Clase para manejar la tabla usuarios de la base de datos. Es clase hija de Validator.
+*	Clase para manejar la tabla de Entradas de la base de datos. Es clase hija de Validator.
 */
 
 class Entradas extends Validator
@@ -19,9 +19,13 @@ class Entradas extends Validator
     /*
     *   Métodos para asignar valores a los atributos.
     */
+
+    //Se valida el Id de la Entrada
     public function setId($value)
     {
+        //Se valida que el campo sea un número natural
         if ($this->validateNaturalNumber($value)) {
+            //Se guarda el dato
             $this->id = $value;
             return true;
         } else {
@@ -29,9 +33,12 @@ class Entradas extends Validator
         }
     }
 
+    //Se valida el Id 
     public function setIdem($value)
     {
+        //Se valida que el campo sea un número natural
         if ($this->validateNaturalNumber($value)) {
+            //Se guarda el dato
             $this->idem = $value;
             return true;
         } else {
@@ -39,9 +46,12 @@ class Entradas extends Validator
         }
     }
 
+    //Se valida la Cantidad a Ingresar 
     public function setCantidad($value)
     {
+        //Se valida que el campo ingresado sea un número natural
         if ($this->validateNaturalNumber($value)) {
+            //Se guarda el dato
             $this->cantidad = $value;
             return true;
         } else {
@@ -49,9 +59,12 @@ class Entradas extends Validator
         }
     }
 
+    //Se valida la Cantidad Actual del inventario
     public function setCantidad_act($value)
     {
+        //Se valida que el campo sea un número natural 
         if ($this->validateNaturalNumber($value)) {
+            //Se guarda el dato
             $this->cantidad_act= $value;
             return true;
         } else {
@@ -59,9 +72,12 @@ class Entradas extends Validator
         }
     }
 
+    //Se valida la Cantidad Sumada 
     public function setSumar($value)
     {
+        //Se valida que el campo sea un número natural
         if ($this->validateNaturalNumber($value)) {
+            //Se guarda el dato
             $this->sumar_cant = $value;
             return true;
         } else {
@@ -69,9 +85,12 @@ class Entradas extends Validator
         }
     }
 
+    //Se valida el Id del Producto
     public function setProducto($value)
     {
+        //Se valida que el campo sea un número natural
         if ($this->validateNaturalNumber($value)) {
+            //Se guarda el dato
             $this->producto = $value;
             return true;
         } else {
@@ -79,9 +98,12 @@ class Entradas extends Validator
         }
     }
 
+    //Se valida el Id del Empleado
     public function setEmpleado($value)
     {
+        //Se valida que el campo sea un número natural
         if ($this->validateNaturalNumber($value)) {
+            //Se guarda el dato
             $this->empleado = $value;
             return true;
         } else {
@@ -89,9 +111,12 @@ class Entradas extends Validator
         }
     }
 
+    //Se valida la Fecha de la Entrada
     public function setFecha($value)
     {
+        //Se valida que la Fecha de la entrada esté en el formato requerido
         if ($this->validateDate($value)) {
+            //Se guarda el dato
             $this->fecha = $value;
             return true;
         } else {
@@ -102,101 +127,142 @@ class Entradas extends Validator
 
     //Métodos para obtener valores de los atributos.
 
+    //Se obtiene el Id de la Entrada
     public function getId()
     {
+        //Se retorna el dato
         return $this->id;
     }
 
+    //Se obtiene el Id 
     public function getIdem()
     {
+        //Se retorna el dato
         return $this->idem;
     }
 
+    //Se obtiene la Cantidad a Ingresar
     public function getCantidad()
     {
+        //Se retorna el dato
         return $this->cantidad;
     }
 
+    //Se obtiene la Cantidad Actual que hay en el Inventario
     public function getCantidad_act()
     {
+        //Se retorna el dato
         return $this->cantidad_act;
     }
 
+    //Se obtiene la suma
     public function getSumar()
     {
+        //Se retorna el dato
         return $this->sumar_cant;
     }
 
+    //Se obtiene el empleado
     public function getEmpleado()
     {
+        //Se retorna el dato
         return $this->empleado;
     }
 
+    //Se obtiene el Id del producto
     public function getProducto()
     {
+        //Se retorna el dato
         return $this->producto;
     }
 
+    //Se obtiene la Fecha de la Entrada
     public function getFecha()
     {
+        //Se retorna el dato
         return $this->fecha;
     }
 
 
     //Métodos para realizar las operaciones SCRUD (search, create, read, update, delete).
+
+    //Leer todas las entradas
     public function readAll()
     {
+        //Se guarda la consulta sql
         $sql = 'SELECT id_entrada, entrada_inventario.cantidad as cantidad, entrada_inventario.fecha as fecha, empleados.nombre as empleado, inventario.nombre as producto, entrada_inventario.id_inventario as id_inventario
                 FROM entrada_inventario INNER JOIN empleados USING(id_empleado)
                 INNER JOIN inventario USING(id_inventario)                
                 ORDER BY entrada_inventario.fecha';
+        //Se guarda un array con los parametros de la consulta(vacío)
         $params = null;
+        //Se retorna la ejecución del método "getRows"
         return Database::getRows($sql, $params);
     }
 
+    //Buscar
     public function searchRows($value)
     {
+        //Se guarda la consulta sql
         $sql = 'SELECT id_entrada, entrada_inventario.cantidad as cantidad, entrada_inventario.fecha as fecha, empleados.nombre as empleado, inventario.nombre as producto, entrada_inventario.id_inventario as id_inventario
         FROM entrada_inventario INNER JOIN empleados USING(id_empleado)
         INNER JOIN inventario USING(id_inventario)      
         WHERE inventario.nombre ILIKE ?
         ORDER BY entrada_inventario.fecha';
+        //Se guarda un array con los parametros de la consulta(vacío)
         $params = array("%$value%");
+        //Se retorna la ejecución del método "getRows"
         return Database::getRows($sql, $params);
     }
 
+    //Leer producto en específico
     public function readProd()
     {
+        //Se guarda la consulta sql
         $sql = 'SELECT id_inventario, nombre as producto
                 FROM inventario';
+        //Se guarda un array con los parametros de la consulta(vacío)
         $params = null;
+        //Se retorna la ejecución del método "getRows"
         return Database::getRows($sql, $params);
     }
 
-
+    //Insert
     public function createRow()
     {
+        //Se guarda la consulta sql
         $sql = 'INSERT INTO entrada_inventario(cantidad, fecha, id_empleado, id_inventario)
                 VALUES(?, ?, ?, ?)';
+        //Se guarda un array con los parametros de la consulta
         $params = array($this->cantidad, $this->fecha, $this->empleado, $this->producto);
+        //Se retorna la ejecución del método "executeRow"
         return Database::executeRow($sql, $params);
     }
 
+    //Actualizar cantidad stock del inventario
     public function updateInventario()
     {
+        //Se guarda la consulta sql
         $sql = 'UPDATE inventario
         SET stock = ?
         WHERE id_inventario = ?';
+        //Se guarda un array con los parametros de la consulta
         $params = array($this->cantidad_act, $this->producto);
+        //Se retorna la ejecución del método "executeRow"
         return Database::executeRow($sql, $params);
     }
 
+    //Leer cantidad actual en stock del inventario
     public function readAct()
     {
+        //Se guarda la consulta sql
         $sql = 'SELECT stock FROM inventario
                 WHERE id_inventario = ?';
+        //Se guarda un array con los parametros de la consulta
         $params = array($this->producto);
-        if ($data = Database::getRow($sql, $params)) {            
+        //Se retorna la ejecución del método "getRow"
+        if ($data = Database::getRow($sql, $params)) { 
+            //Se setea la cantidad en Stock del inventario           
             $this->setSumar($data['stock']);
             return true;
         } else {
@@ -204,12 +270,17 @@ class Entradas extends Validator
         }                
     }
 
+    //Leer al Empleado
     public function readEmpl()
     {
+        //Se guarda la consulta sql
         $sql = 'SELECT id_empleado FROM usuarios
                 WHERE id_usuario = ?';
+        //Se guarda un array con los parametros de la consulta
         $params = array($_SESSION['id_usuario']);
-        if ($data = Database::getRow($sql, $params)) {            
+        //Se retorna la ejecución del método "getRow"
+        if ($data = Database::getRow($sql, $params)) {     
+            //Se setea al empleado       
             $this->setEmpleado($data['id_empleado']);
             return true;
         } else {
@@ -217,11 +288,15 @@ class Entradas extends Validator
         }                
     }
 
+    //Se elimina la Entrada
     public function deleteRow()
     {
+        //Se guarda la consulta sql
         $sql = 'DELETE FROM entrada_inventario
                 WHERE id_entrada = ?';
+        //Se guarda un array con los parametros de la consulta
         $params = array($this->id);
+        //Se retorna la ejecución del método "executeRow"
         return Database::executeRow($sql, $params);
     }
 }
