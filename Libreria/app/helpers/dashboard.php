b3721376043ad57533b58ca02e14ff3037d3bad6
OVillalobos3A <20190474@ricaldone.edu.sv>
2021-10-02 22:43:51 -0600

    Se modificó la plantilla para agregar las vistas de cambio de contraseña y el segundo factor de autentificación

--- Libreria/app/helpers/dashboard.php
+++ Libreria/app/helpers/dashboard.php
@@ -37,7 +37,7 @@ class Dashboard_Page {
     // Se comprueba si existe una sesión de administrador para mostrar el menú de opciones, de lo contrario se muestra un menú vacío.
     if (isset($_SESSION['id_usuario'])) {
         // Se verifica si la página web actual es diferente a index.php (Iniciar sesión) y a register.php (Crear primer usuario) para no iniciar sesión otra vez, de lo contrario se direcciona a main.php
-        if ($filename != 'index.php' && $filename != 'register.php' && $filename != 'primer_uso.php') {
+        if ($filename != 'index.php' && $filename != 'register.php' && $filename != 'primer_uso.php' && $filename != 'changepass.php' && $filename != 'autenticacion.php') {
             // Se llama al método que contiene el código de las cajas de dialogo (modals).
             self::modals();
             // Se imprime el código HTML para el encabezado del documento con el menú de opciones.
@@ -146,7 +146,7 @@ class Dashboard_Page {
         }
     } else {
         // Se verifica si la página web actual es diferente a index.php (Iniciar sesión) y a register.php (Crear primer usuario) para direccionar a index.php, de lo contrario se muestra un menú vacío.
-        if ($filename != 'index.php' && $filename != 'register.php' && $filename != 'primer_uso.php') {
+        if ($filename != 'index.php' && $filename != 'register.php' && $filename != 'primer_uso.php' && $filename != 'changepass.php'  && $filename != 'autenticacion.php') {
             header('location: index.php');
         } else {
             // Se imprime el código HTML para el encabezado del documento con un menú vacío cuando sea iniciar sesión o registrar el primer usuario.
